23c16243ef7a4254eafca31527aa02125d6cf828
nhthanhdn95 <nhthanhdn95@gmail.com>
2023-12-15 09:32:23 +1030

    React Testing Library CodeEvo 35 onward

---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react"
+import { Greet } from "./greet"
+
+describe('Greet', () => {
+  test('renders correctly', () => {
+    render(<Greet />);
+    const textElement = screen.getByText('Hello');
+    expect(textElement).toBeInTheDocument();
+  })
+  
+  test('renders a name', () => {
+    render(<Greet name='Phao'/>);
+    const textElement = screen.getByText('Hello Phao');
+    expect(textElement).toBeInTheDocument();
+  })
+})
